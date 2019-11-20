bb3f5383f2262256ded81906a7cb4814a6169ab6
Mario Adamo Jr <marioadamojr@gmail.com>
2019-11-20 23:58:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+  let content = document.getElementById('text')
+    content.innerHTML = "This is really cool!";
+});
